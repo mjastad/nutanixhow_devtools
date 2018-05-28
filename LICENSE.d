df26451707413d8d2beadb9f82fb2afb9ce85f87
Michael Jastad <cattfish00@gmail.com>
2018-05-28 16:10:48 -0700

    Create LICENSE.d

---
+++ LICENSE.d
@@ -0,0 +1,3 @@
+Copyright (C) 2018 Michael Jastad (michael.jastad@nutanix.com)
+
+This work is licensed under the Creative Commons Attribution 3.0 Unported License. To view a copy of this license, visit http://creativecommons.org/licenses/by/3.0/deed.en_US.
